30dc2bfae8ec668042d6ce09585ebe3bee119beb
PoloLavapies <iberia9lavapies@gmail.com>
2025-01-17 21:38:31 +0900

    headless

--- x_ad_block.py
+++ x_ad_block.py
@@ -91,7 +91,7 @@ def mute():
         mute()
 
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
